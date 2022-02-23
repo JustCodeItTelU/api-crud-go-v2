14821d82575bbced7b75002f58747bf68564169c
Hafiz Faruqi <faruqihafiz21@gmail.com>
2022-02-23 16:13:11 +0700

    fix error handling

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"JCI-Go-API/database"
 	"JCI-Go-API/routes"
 	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
 	routes.Setup(app)
-	err := app.Listen("3000")
+	err := app.Listen(":3000")
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
